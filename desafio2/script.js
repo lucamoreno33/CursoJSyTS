76846b0d7418b2a5b410497db7e0b6e950f583ef
luca <luca_moreno2010@hotmail.com>
2025-08-27 16:47:43 -0300

    desafio1 simplificado

--- desafio2/script.js
+++ desafio2/script.js
@@ -1,7 +1,8 @@
-const calcularAreaTriangulo = (base, altura) => {
-    const area = ((base * altura) /2)
-    return area
-}
+const calcularAreaTriangulo = (base, altura) => ((base * altura) /2) // captura el resultado de la cuenta y lo retorna
 
-console.log(calcularAreaTriangulo(4, 6))
+
+console.log(calcularAreaTriangulo(4, 6)) //lo llamo con console log por que si no solo 
+                                        // tira el return al aire
+
+
 
